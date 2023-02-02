42de7754bbaeff23005e90907f4512bf639d9567
Taylor Smith <smith.taylord@gmail.com>
2023-02-01 19:10:16 -0700

    Houses complete, Jobs can draw to page

--- app/Models/Job.js
+++ app/Models/Job.js
@@ -1,3 +1,73 @@
+import { generateId } from "../Utils/generateId.js"
 
 
-export class Job { }
+export class Job {
+
+    constructor(data) {
+        this.id = data.id || generateId()
+        this.title = data.title
+        this.company = data.company
+        this.salary = data.salary
+        this.location = data.location
+        this.imgUrl = data.imgUrl
+        this.description = data.description
+    }
+
+    get JobCardTemplate() {
+        return /*html*/ `
+        <div class="col-md-4 my-3">
+    <div class="card elevation-2 car" onclick="app.jobsController.setActiveJob('${this.id}')" data-bs-toggle="modal" data-bs-target="#listingModal">
+        <img
+        src="${this.imgUrl}"
+        alt="${this.title}" class="rounded">
+        <p><b>${this.title} at ${this.company} - $${this.salary} Salary</b></p>
+    </div>
+    </div>
+        `
+    }
+
+    static JobForm() {
+        return /*html*/`
+        <form onsubmit="app.jobsController.handleFormSubmit()">
+    
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="title" required minlength="3" maxlength="20">
+                <label for="title">Title</label>
+            </div>
+    
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="company" required>
+                <label for="company">Company</label>
+            </div>
+    
+            <div class="form-floating mb-3">
+                <input type="number" class="form-control" name="year" required min="1886" max="9999">
+                <label for="year">Year</label>
+            </div>
+    
+            <div class="form-floating mb-3">
+                <input type="number" class="form-control" name="price" required min="0">
+                <label for="price">Price</label>
+            </div>
+    
+            <div class="form-floating mb-3">
+                <input type="url" class="form-control" name="imgUrl">
+                <label for="imgUrl">Image Url <i>(We are too lazy for uploads)</i></label>
+            </div>
+    
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Describe your Listing" name="description"></textarea>
+                <label for="description">Description</label>
+            </div>
+    
+            <div class="d-flex my-4 gap-5 align-items-center">
+                <button class="btn" type="reset">Cancel</button>
+                <button class="btn btn-primary" type="submit" data-bs-dismiss="offcanvas">Submit</button>
+            </div>
+    
+            </form>
+        `
+
+    }
+
+}
